3ca6b857b04bd691ba2226be1f55aec0ada30f98
dmostowfi <deanna.mostowfi@kellogg.northwestern.edu>
2022-05-16 16:05:21 +0000

    create post action in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+
+    def index
+        @posts = Post.all
+    end
+
+    def create #actually need to check if this is the right approach. not sure if params has the last 4
+        @post = Post.new
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["post"]["place_id"]
+        @post["created_at"] = params["post"]["created_at"]
+        @post["updated_at"] = params["post"]["updated_at"]
+        @post.save
+        redirect_to "/posts"
+      end
+
 end
