4fd100645bf832fe7051b6e544b2d27e2aa00f46
dmostowfi <deanna.mostowfi@kellogg.northwestern.edu>
2022-05-16 16:30:52 +0000

    create place action in controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,4 +4,13 @@ class PlacesController < ApplicationController
         @places = Place.all
     end
 
+    def create #actually need to check if this is the right approach. not sure if params has the last 2
+        @place = Place.new
+        @place["name"] = params["place"]["name"]
+        @place["created_at"] = params["place"]["created_at"]
+        @place["updated_at"] = params["place"]["updated_at"]
+        @post.save
+        redirect_to "/places"
+      end
+
 end
