9fcbe8c0a02f45ea38bf853be75278df6812e33d
dmostowfi <deanna.mostowfi@kellogg.northwestern.edu>
2022-05-17 21:23:39 +0000

    created relationships

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,15 +6,17 @@ class PostsController < ApplicationController
 
     def new
         @post = Post.new
+        @place = Place.find_by({ "id" => params["place_id"] })
+        @post["place_id"] = @place["id"]
     end
 
     def create 
         @post = Post.new
         @post["title"] = params["post"]["title"]
         @post["description"] = params["post"]["description"]
-        @post["place_id"] = Place.find_by({ "id" => params["id"] })
+        @post["place_id"] = params["post"]["place_id"]
         @post.save
-        redirect_to "/places" #Need to figure out how to redirect this back to a specific place 
+        redirect_to "/places/#{@post["place_id"]}" #Redirects back the place that the post is for 
     end
 
 end
