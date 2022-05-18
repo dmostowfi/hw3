69ea2e2121b5f7eba7e35c50e84cf9a7cd26ba3a
dmostowfi <deanna.mostowfi@kellogg.northwestern.edu>
2022-05-18 01:13:46 +0000

    added posted_on

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,8 +5,11 @@ class PlacesController < ApplicationController
     end
 
     def show
-        @place = Place.find_by({ "id" => params["id"] })
-        @posts = Post.where({"place_id" => @place["id"]})
+        @place = Place.find_by({ "id" => params["id"] }) #params ID comes from URL, and find_by retrieves it from places table
+        #findby is when there's only one result 
+        #where is when you expect multiple; why variable name is plural
+        @posts = Post.where({"place_id" => @place["id"]}) #for field place_id in posts table, return data where it's @place[id]
+        #Post.destroy_all
     end
 
     def new
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,16 @@
 class PostsController < ApplicationController
 
-    def index
-        @posts = Post.all
-    end
-
-    def new
+    def new #new just displays thef form; create actually performs the logic
         @post = Post.new
-        @place = Place.find_by({ "id" => params["place_id"] })
-        @post["place_id"] = @place["id"]
+        @post["place_id"] = params["place_id"] #params place_id comes from query params in place show page ?place_id=
+        @place = Place.find_by({ "id" => params["place_id"]}) #place_id is the value you're providing to look up in Places table (for new post page)
     end
 
     def create 
         @post = Post.new
         @post["title"] = params["post"]["title"]
         @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
         @post["place_id"] = params["post"]["place_id"]
         @post.save
         redirect_to "/places/#{@post["place_id"]}" #Redirects back the place that the post is for 
