5b87f2372c3d5172d92fba42641f2772f3d421a9
dmostowfi <deanna.mostowfi@kellogg.northwestern.edu>
2022-05-19 16:43:05 +0000

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources "posts"
   resources "places"
-  # Defines the root path route ("/")
-  get("/", { :controller => "articles", :action => "index" })
+  # Defines the root path route ("/"). AKA where you want the user to go first
+  get("/", { :controller => "places", :action => "index" })
 end
